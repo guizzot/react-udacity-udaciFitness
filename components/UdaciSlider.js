38600a1eb0f130721a14e1517996a8d6260abf65
Guilherme Ribeiro <43709413+guizzot@users.noreply.github.com>
2019-03-20 21:20:33 -0300

    Add components
    New components and adjusts in utils/helpers.js

---
+++ components/UdaciSlider.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View,Text } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
+
+export default function UdaciSlider () {
+    return (
+        <View>
+            <Text>
+                UdaciSlider
+            </Text>
+        </View>
+    )
+}
---
+++ components/UdaciSteppers.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View,Text } from 'react-native'
+import { getMetricMetaInfo } from '../utils/helpers'
+
+export default function UdaciSteppers () {
+    return (
+        <View>
+            <Text>
+                UdaciSteppers
+            </Text>
+        </View>
+    )
+}
