93c604020faaab8b6a2af05acf71e38cb2414466
Guilherme Ribeiro <43709413+guizzot@users.noreply.github.com>
2019-03-24 13:10:21 -0300

    Add/Adjusts new/current components

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
-import { getMetricMetaInfo } from '../utils/helpers'
+import { View, TouchableOpacity,Text } from 'react-native'
+import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
+import DateHeader from './DateHeader'
+import TextButton from './TextButton'
+import { Ionicons } from '@expo/vector-icons'
+
+
+function SubmitBtn({ onPress }){
+    return (
+        <TouchableOpacity
+            onPress={onPress}
+        >
+            <Text>
+                Submit
+            </Text>
+        </TouchableOpacity>
+    )
+}
 
 export default class AddEntry extends Component {
 
@@ -46,11 +62,58 @@ export default class AddEntry extends Component {
         }))
     }
 
+    submit = () => {
+        const key = timeToString()
+        const entry = this.state
+
+        //Update Redux
+
+        this.setState(() => ({
+            run:0,
+            bike:0,
+            swim:0,
+            sleep:0,
+            eat:0,
+        }))
+
+        //Navigate to Home
+        //Save to DB
+        //Clearn local notification
+    }
+
+    reset = () => {
+        const key = timeToString()
+
+        //Update Redux
+
+        //Route to Home
+
+        //Update DB
+    }
+
     render() {
         const metaInfo = getMetricMetaInfo()
+        
+        if(this.props.alreadyLogged){
+            return (
+                <View>
+                    <Ionicons 
+                        name='ios-happy'
+                        size={100}
+                    />
+                    <Text>
+                        You already logged your information for today
+                    </Text>
+                    <TextButton onPress={this.reset} >
+                        Reset
+                    </TextButton>
+                </View>
+            )
+        }
 
         return (
             <View>
+            <DateHeader date={(new Date()).toLocaleDateString()} />
                 {
                     Object.keys(metaInfo).map((key) => {
                         const {getIcon, type, ...rest} = metaInfo[key]
@@ -77,7 +140,8 @@ export default class AddEntry extends Component {
                         )
 
                     })
-                }        
+                }   
+                <SubmitBtn onPress={this.submit} />     
             </View>
         )
     }
--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
-import { View,Text } from 'react-native'
+import { View, Text, Slider } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
 
-export default function UdaciSlider () {
+export default function UdaciSlider ({max,unit,step,value,onChange}) {
     return (
         <View>
-            <Text>
-                UdaciSlider
-            </Text>
+            <Slider 
+                step={step}
+                value={value}
+                maximumValue={max}
+                minimumValue={0}
+                onValueChange={onChange}
+            />
+            <View>
+                <Text>
+                    {value}
+                </Text>
+                <Text>
+                    {unit}
+                </Text>
+            </View>
         </View>
     )
 }
